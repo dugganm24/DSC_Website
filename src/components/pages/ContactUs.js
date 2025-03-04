3c262654c9e2faf3499285ca2e2fe1377cfb5aa5
Michael Duggan <mpduggan@wpi.edu>
2025-03-04 18:18:12 -0500

    some background formatting

--- src/components/pages/ContactUs.js
+++ src/components/pages/ContactUs.js
@@ -4,11 +4,13 @@ import './ContactUs.css';
 
 const ContactUs = () => {
   return (
-    <div className="contact-us-container">
+   <div className="contact-us-wrapper">
+   <div className="contact-us-container">
       <h1>Contact Us</h1>
       <div className="contact-us-form">
         <ContactForm />
       </div>
+    </div>
     </div>
   );
 };
